8944020396b8fe06bf1984d4312c8904ca5a9177
Miles Cragg <milescragg@gmail.com>
2022-11-14 16:51:11 +0000

    Next test passes: withdrawal can be made from account

--- jest/accountTransaction.test.js
+++ jest/accountTransaction.test.js
@@ -2,10 +2,19 @@ const AccountTransaction = require('../lib/accountTransaction.js');
 
 describe('Depositing into account', () => {
   it('returns "1000"', () => {
-
-  const transaction = new AccountTransaction();
-  transaction.getDeposit(1000)
-  const result = transaction.displayTransaction().deposit;
-  expect(result).toEqual(1000);
+    const transaction = new AccountTransaction();
+    transaction.getDeposit(1000)
+    const result = transaction.displayTransaction().deposit;
+    expect(result).toEqual(1000);
   });
 });
+
+describe('Withdrawing from account', () => {
+  it('returns "1000"', () => {
+    const transaction = new AccountTransaction();
+    transaction.getWithdrawal(1000)
+    const result = transaction.displayTransaction().withdrawal;
+    expect(result).toEqual(1000);
+  });
+});
+

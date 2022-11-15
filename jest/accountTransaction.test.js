0baa3cdb0d1c22f8b9344e1220820298e96ee257
Miles Cragg <milescragg@gmail.com>
2022-11-15 11:17:27 +0000

    adds date as 'DD/MM/YYYY' to transaction

--- jest/accountTransaction.test.js
+++ jest/accountTransaction.test.js
@@ -18,3 +18,18 @@ describe('Withdrawing from account', () => {
   });
 });
 
+describe('Transaction date added to object', () => {
+  it('returns date matching regex', () => {
+    const transaction = new AccountTransaction();
+    const currentTime = new Date();
+    const day = currentTime.getDate();
+    const month = currentTime.getMonth() + 1;
+    const year = currentTime.getUTCFullYear();
+    const testResult = `${day}/${month}/${year}`
+    transaction.getDate()
+    const result = transaction.displayTransaction().date;
+    expect(result).toEqual(testResult);
+  });
+});
+
+
